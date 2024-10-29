3df0676e8a75c4de905d45d2466218dc71c9ac2a
Abishek Anthony <54862087+abishekanthony07@users.noreply.github.com>
2024-10-29 14:54:50 +0100

    update project

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@
 module.exports = function(grunt) {
 	var port = grunt.option('port') || 8000;
 	var root = grunt.option('root') || '.';
-
 	if (!Array.isArray(root)) root = [root];
 
 	// Project configuration
@@ -18,11 +17,9 @@ module.exports = function(grunt) {
 				' * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n' +
 				' */'
 		},
-
 		qunit: {
 			files: [ 'test/*.html' ]
 		},
-
 		uglify: {
 			options: {
 				banner: '<%= meta.banner %>\n',
@@ -33,7 +30,6 @@ module.exports = function(grunt) {
 				dest: 'js/reveal.min.js'
 			}
 		},
-
 		sass: {
 			core: {
 				src: 'css/reveal.scss',
@@ -47,13 +43,16 @@ module.exports = function(grunt) {
 				ext: '.css'
 			}
 		},
-
-		autoprefixer: {
+		postcss: { // Replace autoprefixer with postcss
+			options: {
+				processors: [
+					require('autoprefixer')() // Add vendor prefixes
+				]
+			},
 			core: {
 				src: 'css/reveal.css'
 			}
 		},
-
 		cssmin: {
 			options: {
 				compatibility: 'ie9'
@@ -63,7 +62,6 @@ module.exports = function(grunt) {
 				dest: 'css/reveal.min.css'
 			}
 		},
-
 		jshint: {
 			options: {
 				curly: false,
@@ -89,7 +87,6 @@ module.exports = function(grunt) {
 			},
 			files: [ 'Gruntfile.js', 'js/reveal.js' ]
 		},
-
 		connect: {
 			server: {
 				options: {
@@ -101,7 +98,6 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-
 		zip: {
 			bundle: {
 				src: [
@@ -116,7 +112,6 @@ module.exports = function(grunt) {
 				dest: 'reveal-js-presentation.zip'
 			}
 		},
-
 		watch: {
 			js: {
 				files: [ 'Gruntfile.js', 'js/reveal.js' ],
@@ -145,48 +140,38 @@ module.exports = function(grunt) {
 				livereload: true
 			}
 		},
-
 		retire: {
 			js: [ 'js/reveal.js', 'lib/js/*.js', 'plugin/**/*.js' ],
 			node: [ '.' ]
 		}
-
 	});
 
 	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-autoprefixer' );
-	grunt.loadNpmTasks( 'grunt-retire' );
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-zip' );
-	
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-retire');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-zip');
+	grunt.loadNpmTasks('grunt-postcss'); // Load grunt-postcss
+
 	// Default task
-	grunt.registerTask( 'default', [ 'css', 'js' ] );
-
+	grunt.registerTask('default', ['css', 'js']);
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
-
+	grunt.registerTask('js', ['jshint', 'uglify', 'qunit']);
 	// Theme CSS
-	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
-
+	grunt.registerTask('css-themes', ['sass:themes']);
 	// Core framework CSS
-	grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
-
+	grunt.registerTask('css-core', ['sass:core', 'postcss:core', 'cssmin']);
 	// All CSS
-	grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
-
+	grunt.registerTask('css', ['sass', 'postcss', 'cssmin']);
 	// Package presentation to archive
-	grunt.registerTask( 'package', [ 'default', 'zip' ] );
-
+	grunt.registerTask('package', ['default', 'zip']);
 	// Serve presentation locally
-	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
-
+	grunt.registerTask('serve', ['connect', 'watch']);
 	// Run tests
-	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
-
+	grunt.registerTask('test', ['jshint', 'qunit']);
 };
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Kotlin Workshop
+
+## Pre-requisite
+
+> - Install node.js
+> - execute ```npm install```
+
 # Learn Kotlin for Zuehlke
 
 Find the Presentation on https://kotlinworkshopzuhlke.github.io/KotlinWorkshopPresentation/
+
