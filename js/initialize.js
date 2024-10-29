e592443d74e7e6f71cbb2fa2141754a36cb2af11
Abishek Anthony <54862087+abishekanthony07@users.noreply.github.com>
2024-10-29 17:48:58 +0100

    internal - [structure] restructure html content for better accessability / readability

---
+++ js/initialize.js
@@ -0,0 +1,44 @@
+// More info https://github.com/hakimel/reveal.js#configuration
+
+Reveal.initialize({
+    history: true,
+
+    // More info https://github.com/hakimel/reveal.js#dependencies
+    dependencies: [
+        {src: '../../plugin/markdown/marked.js'},
+        {src: '../../plugin/markdown/markdown.js'},
+        {src: '../../plugin/notes/notes.js', async: true},
+        {
+            src: '../../plugin/highlight/highlight.js', async: true, callback: function () {
+                hljs.initHighlightingOnLoad();
+            }
+        }
+    ]
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loadExternalHTML = async () => {
+        // Find all elements with the attribute data-external
+        const externalElements = document.querySelectorAll('[data-external]');
+
+        // Loop through each element and load the external HTML file
+        for (let element of externalElements) {
+            const filePath = "topic/" + element.getAttribute('data-external');
+
+            try {
+                const response = await fetch(filePath);
+                if (response.ok) {
+                    const content = await response.text();
+                    element.innerHTML = content;
+                } else {
+                    console.error(`Error loading ${filePath}: ${response.statusText}`);
+                }
+            } catch (error) {
+                console.error(`Fetch error for ${filePath}:`, error);
+            }
+        }
+    };
+
+    // Call the function to load external HTML
+    loadExternalHTML();
+});
