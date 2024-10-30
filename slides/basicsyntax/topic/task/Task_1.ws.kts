9105fb51b39064c881b4c0099e60459b03241013
Abishek Anthony <54862087+abishekanthony07@users.noreply.github.com>
2024-10-30 15:19:24 +0100

    Add results of tasks!

--- slides/basicsyntax/topic/task/Task_1.ws.kts
+++ slides/basicsyntax/topic/task/Task_1.ws.kts
@@ -7,3 +7,32 @@
         - Boolean (default to true) isActive
  */
 
+class UserExampleOne(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val isActive: Boolean,
+)
+
+open UserExampleOne(
+    open val id: Int,
+    open val name: String,
+    open val email: String,
+    open val isActive: Boolean,
+)
+
+data class User(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val isActive: Boolean,
+)
+
+fun main() {
+    val userOne = UserExampleOne(1, "Bob", "bob.alice@zuehlke.com", false)
+    val userTwo = User(1, "Alice", "bob.alice@zuehlke.com", false)
+
+    println(userOne)
+    println(userTwo)
+}
+main()
